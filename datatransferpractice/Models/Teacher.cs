e154ad2af75ac2bd1d9ca5ac0c005eea6266b67f
masabmunir <masabk214@gmail.com>
2025-01-15 19:38:03 +0500

    installing packages and working on code first approach

---
+++ datatransferpractice/Models/Teacher.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace datatransferpractice.Models
+{
+    public class Teacher
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column ("TeacherName", TypeName = "varchar(100)")]
+        public string Name { get; set; }
+        [Column ("TeacherGender", TypeName = "varchar(20)")]
+        public string Gender { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ datatransferpractice/Models/TeacherDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace datatransferpractice.Models
+{
+    public class TeacherDBContext : DbContext
+    {
+        public TeacherDBContext( DbContextOptions options) :base(options)
+        {
+                
+        }
+
+       public DbSet<Teacher> Teachers { get; set; }   
+    }
+}
