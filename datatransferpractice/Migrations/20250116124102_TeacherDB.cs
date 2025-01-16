fb4b5a5cbd21e36f6061a02201cc1fe4ef373afa
masabmunir <masabk214@gmail.com>
2025-01-16 17:55:19 +0500

    Added database connection

---
+++ datatransferpractice/Migrations/20250116124102_TeacherDB.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace datatransferpractice.Migrations
+{
+    /// <inheritdoc />
+    public partial class TeacherDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Teachers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TeacherName = table.Column<string>(type: "varchar(100)", nullable: false),
+                    TeacherGender = table.Column<string>(type: "varchar(20)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teachers", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Teachers");
+        }
+    }
+}
--- datatransferpractice/Program.cs
+++ datatransferpractice/Program.cs
@@ -1,9 +1,17 @@
+using datatransferpractice.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<TeacherDBContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("dbcs")));
+
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
